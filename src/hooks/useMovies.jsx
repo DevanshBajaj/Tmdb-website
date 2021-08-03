4a57cdd8b9952a4469db17eeccd1c5dee881a10f
Devansh Bajaj <devansh21db.db@gmail.com>
2021-08-04 00:51:19 +0530

    feat: custom hook to manage state and API request

---
+++ src/hooks/useMovies.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function useMovies(pageNumber) {
+	const apiKey = import.meta.env.VITE_API_KEY;
+
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+	const [movies, setMovies] = useState([]);
+	const [hasMore, setHasMore] = useState(false);
+
+	useEffect(() => {
+		setLoading(true);
+		setError(false);
+		let cancel;
+		axios({
+			method: "GET",
+			url: "https://api.themoviedb.org/3/movie/top_rated?",
+			params: {
+				api_key: apiKey,
+				language: "en",
+				region: "us",
+				page: pageNumber,
+			},
+			cancelToken: new axios.CancelToken((c) => (cancel = c)),
+		})
+			.then((response) => {
+				setMovies((prevMovies) => {
+					return [...new Set([...prevMovies, ...response.data.results])];
+				});
+				setHasMore(response.data.results.length > 0);
+				setLoading(false);
+				console.log(response.data);
+			})
+			.catch((error) => {
+				if (axios.isCancel(error)) return;
+				setError(true);
+			});
+		return () => cancel();
+	}, [pageNumber]);
+
+	return { loading, error, movies, hasMore };
+}
