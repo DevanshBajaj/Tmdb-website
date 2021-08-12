fdd97c5e1a0313467226c761df37ab0b3f413456
Devansh Bajaj <Devansh21db.db@gmail.com>
2021-08-13 02:38:27 +0530

    chore: add lottie file & component for error page

---
+++ src/Lottie.jsx
@@ -0,0 +1,20 @@
+import Lottie from "react-lottie";
+
+const LottieAnimation = ({ lotti, width, height }) => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: lotti,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <div>
+      <Lottie options={defaultOptions} height={height} width={width} />
+    </div>
+  );
+};
+
+export default LottieAnimation;
